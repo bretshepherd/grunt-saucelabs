776f12e449414d98e348bf4cc8efaaf3ecd84d75
Jason Risch <jason.risch@swiftpage.com>
2013-12-16 14:43:01 -0700

    Rework the specrunner timeout to wait for up to 1 minute on processing results

--- tasks/saucelabs.js
+++ tasks/saucelabs.js
@@ -227,15 +227,21 @@ module.exports = function(grunt) {
             var el = els[0];
             var retryCount = 0;
 
-            var fetchResults = function(cb, status,retries) {
-			  retries = typeof retries !== 'undefined' ? retries +1 : 0;
-			  if (retries>3)
-				return;
-              driver.safeEval("jasmine.getJSReport ? jasmine.getJSReport() : null;", function(err, obj) {
-				if(obj==null){
-					fetchResults(cb,status,retries);
-					return;
-				}
+            var fetchResults = function (cb, status)
+            {
+                var fn="var result; \
+                    function checkVariable() { \
+                    if ((jasmine.getJSReport ? jasmine.getJSReport() : null) != null) \
+                    { \
+                        result=jasmine.getJSReport(); \
+                    } \
+                    else { \
+                        window.setTimeout('checkVariable();', 60000); \
+                    } \
+                }; \
+                checkVariable();\
+                result";
+              driver.safeEval(fn, function(err, obj) {
                 cb(status, obj);
               });
             };
